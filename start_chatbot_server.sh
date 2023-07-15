0a4d60e03d26ed17b88998833faf60468ab083f3
Ashley Kleynhans <ashley.kleynhans@gmail.com>
2023-07-15 20:25:40 +0200

    Proxy API via nginx

--- start_chatbot_server.sh
+++ start_chatbot_server.sh
@@ -2,4 +2,10 @@
 
 source /workspace/venv/bin/activate
 cd /workspace/text-generation-webui
-python server.py --listen --listen-port 3001 --api --chat
+python server.py \
+  --listen \
+  --api \
+  --listen-port 3001 \
+  --api-blocking-port 5001 \
+  --api-streaming-port 5006 \
+  --chat
